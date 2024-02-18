5fa21747d26beeaf75d4ff67cf6d6f3dcd88d797
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2024-02-18 15:46:38 +0300

    test with port

--- connector.py
+++ connector.py
@@ -4,7 +4,6 @@ import os
 SFTP_USER = os.environ["sftp-user"] 
 SFTP_PASS = os.environ["sftp-pass"]
 SFTP_HOST = os.environ["sftp-host"]
-SFTP_PORT = os.environ["sftp-port"]
 
 print(SFTP_HOST)
 def create_sftp_client(host, port, username, password):
@@ -19,6 +18,6 @@ def download_file_from_server(sftp_client, remote_file, local_file):
     sftp_client.get(remote_file, local_file)
 
 
-sftp_client = create_sftp_client(SFTP_HOST, SFTP_PORT, SFTP_USER, SFTP_PASS)
+sftp_client = create_sftp_client(SFTP_HOST, 50, SFTP_USER, SFTP_PASS)
 download_file_from_server(sftp_client, "ml_model/synology.pub", "synology.txt")
 sftp_client.close()
