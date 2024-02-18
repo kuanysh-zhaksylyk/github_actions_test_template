06fad45d6b935f8ddbc40beab487f4bedbf9a76f
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2024-02-18 16:57:05 +0300

    test finally

--- connector.py
+++ connector.py
@@ -4,11 +4,8 @@ import os
 SFTP_USER = os.environ["sftp-user"] 
 SFTP_PASS = os.environ["sftp-pass"]
 SFTP_HOST = os.environ["sftp-host"]
+SFTP_PORT = int(os.environ["sftp-port"])  
 
-SFTP_USER = os.environ.get("sftp-user")
-if SFTP_USER is None:
-    print("Variable 'sftp-user' is not set.")
-    # Обработка ошибки или завершение скрипта
 
 def create_sftp_client(host, port, username, password):
     transport = paramiko.Transport((host, port))
@@ -22,6 +19,6 @@ def download_file_from_server(sftp_client, remote_file, local_file):
     sftp_client.get(remote_file, local_file)
 
 
-sftp_client = create_sftp_client(SFTP_HOST, 50, SFTP_USER, SFTP_PASS)
+sftp_client = create_sftp_client(SFTP_HOST, SFTP_PORT, SFTP_USER, SFTP_PASS)
 download_file_from_server(sftp_client, "ml_model/synology.pub", "synology.txt")
 sftp_client.close()
