58fce259bc512325c5af39b30fee1470df8c2b25
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2024-02-18 15:43:48 +0300

    test ci with all right

--- connector.py
+++ connector.py
@@ -1,24 +1,24 @@
 import paramiko
 import os 
 
-SFTP_USER = os.environ.get("sftp-user")
-if SFTP_USER is None:
-    print("Variable 'sftp-user' is not set.")
-    
-# Обработка ошибки или завершение скрипта
+SFTP_USER = os.environ["sftp-user"] 
+SFTP_PASS = os.environ["sftp-pass"]
+SFTP_HOST = os.environ["sftp-host"]
+SFTP_PORT = os.environ["sftp-port"]
 
-# def create_sftp_client(host, port, username, password):
-#     transport = paramiko.Transport((host, port))
-#     transport.connect(username=username, password=password)
+print(SFTP_HOST)
+def create_sftp_client(host, port, username, password):
+    transport = paramiko.Transport((host, port))
+    transport.connect(username=username, password=password)
  
-#     sftp_client = paramiko.SFTPClient.from_transport(transport)
+    sftp_client = paramiko.SFTPClient.from_transport(transport)
  
-#     return sftp_client
+    return sftp_client
 
-# def download_file_from_server(sftp_client, remote_file, local_file):
-#     sftp_client.get(remote_file, local_file)
+def download_file_from_server(sftp_client, remote_file, local_file):
+    sftp_client.get(remote_file, local_file)
 
 
-# sftp_client = create_sftp_client(SFTP_HOST, SFTP_PORT, SFTP_USER, SFTP_PASS)
-# download_file_from_server(sftp_client, "ml_model/synology.pub", "synology.txt")
-# sftp_client.close()
+sftp_client = create_sftp_client(SFTP_HOST, SFTP_PORT, SFTP_USER, SFTP_PASS)
+download_file_from_server(sftp_client, "ml_model/synology.pub", "synology.txt")
+sftp_client.close()
