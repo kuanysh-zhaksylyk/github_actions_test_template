1f71d490ac7ebad74d2abee157b55ad87a654b81
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2024-02-18 15:08:14 +0300

    test: ci with secrets in py file

--- connector.py
+++ connector.py
@@ -1,5 +1,11 @@
 import paramiko
- 
+import os 
+
+SFTP_USER = os.environ["sftp-user"] 
+SFTP_PASS = os.environ["sftp-pass"]
+SFTP_HOST = os.environ["sftp-host"]
+SFTP_PORT = os.environ["sftp-port"]
+
 def create_sftp_client(host, port, username, password):
     transport = paramiko.Transport((host, port))
     transport.connect(username=username, password=password)
@@ -12,6 +18,6 @@ def download_file_from_server(sftp_client, remote_file, local_file):
     sftp_client.get(remote_file, local_file)
 
 
-sftp_client = create_sftp_client("iss-nas.synology.me", 50, "user-guest", "syn#OStis2019")
+sftp_client = create_sftp_client(SFTP_HOST, SFTP_PORT, SFTP_USER, SFTP_PASS)
 download_file_from_server(sftp_client, "ml_model/synology.pub", "synology.txt")
 sftp_client.close()
